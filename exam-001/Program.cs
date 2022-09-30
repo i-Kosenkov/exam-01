2a057f2cf829955339147bce11cb9c1f6896784a
Kosenkov <9588434@gmail.com>
2022-09-30 15:08:20 +0300

    Add metod GetArrayLessThreeChar

--- exam-001/Program.cs
+++ exam-001/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: из имеющего массива строк сформировать массив из строк длина которых <= 3 символам
+//Массив с данными заполняет программа
+
+string[] inputArray = new string[] { "hello", "2", "world", ":-)" };
+PrintArray(GetArrayLessThreeChar(inputArray));
+
+//Метод получения массива состоящего из строк с длиной <= 3
+string[] GetArrayLessThreeChar(string[] array)
+{
+    int lenghtNewArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            if (array[i].Length <= 3)
+            {
+                lenghtNewArray++;
+            }
+        }
+    }
+
+    string[] newArray = new string[lenghtNewArray];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
